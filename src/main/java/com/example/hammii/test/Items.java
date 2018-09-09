1f683aed4acefe52c9edf75ff4f1e2e8a03fabc4
hammadpirzada1 <40149615+hammadpirzada1@users.noreply.github.com>
2018-09-09 09:34:10 -0700

    Add files via upload

---
+++ src/main/java/com/example/hammii/test/Items.java
@@ -0,0 +1,19 @@
+package com.example.hammii.test;
+
+public class Items {
+
+    private String title;
+
+    // private String description;
+
+    public Items(String title) {
+        super();
+        this.title = title;
+
+    }
+
+    public String getText() {
+        return this.title;
+    }
+    // getters and setters...
+}
---
+++ src/main/java/com/example/hammii/test/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.hammii.test;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private PhotoSortrView photoSorter;
+    private Button addButton, removeButton;
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        photoSorter = (PhotoSortrView) findViewById(R.id.photosortr);
+        addButton = (Button)findViewById(R.id.btnAdd);
+        removeButton = (Button)findViewById(R.id.btnRemove);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                photoSorter.addImages(MainActivity.this, R.drawable.line);
+            }
+        });
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                photoSorter.removeImage();
+            }
+        });
+    }
+
+
+
+}
